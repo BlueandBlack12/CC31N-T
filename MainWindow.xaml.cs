45de4872a04016db2d9c939e641c38555a0637d4
BlueandBlack12 <jamesjrpat@gmail.com>
2024-03-22 00:00:13 +0800

    Initial Commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,9 +24,7 @@ public partial class MainWindow : Window
         _gameSession = new GameSession();
         DataContext = _gameSession;
     }
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-    {
-        _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
-    }
+  
+    
 }
 
